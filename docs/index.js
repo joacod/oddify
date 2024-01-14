c1d543e3d08bf54b0e964e1e3c83632d30a54752
Joaquin Diaz <joacod@gmail.com>
2024-01-15 00:27:58 +0100

    astro configs and docs

--- docs/index.js
+++
@@ -1,7 +0,0 @@
-import { Button } from "../index.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const appContainer = document.getElementById("app");
-  const button = new Button("Click me");
-  button.render(appContainer);
-});
